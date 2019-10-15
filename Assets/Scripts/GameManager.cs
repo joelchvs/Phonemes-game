825c53100be51c1e5047beb57ee052167986f5f6
Joel Chaves <joelchvs93@gmail.com>
2019-10-15 02:11:43 -0600

    update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public Phoneme[] phonemes;
+    private static List<Phoneme> unansweredPhonemes;
+    private Phoneme currentPhoneme;
+
+    void Start()
+    {
+        if(unansweredPhonemes == null || unansweredPhonemes.Count == 0)
+        {
+            unansweredPhonemes = phonemes.ToList<Phoneme>();
+        }
+        SetRandomPhoneme();
+    }
+
+    void SetRandomPhoneme()
+    {
+        int randomPhonemeIndex = Random.Range(0, unansweredPhonemes.Count);
+        currentPhoneme = unansweredPhonemes[randomPhonemeIndex];
+
+        unansweredPhonemes.RemoveAt(randomPhonemeIndex);
+    }
+
+}
---
+++ Assets/Scripts/Phoneme.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Phoneme
+{
+ public string phoneme;
+ public AudioClip phonemeSound;
+ public string[] correctAnswers;
+
+
+
+}
